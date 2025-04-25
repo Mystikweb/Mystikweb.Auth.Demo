7c1b73041c423cd556b835587190c7823408e657
Christopher Hair <mystikweb@live.ca>
2025-04-25 17:15:44 -0400

    Adding infrastructure constants

--- src/Mystikweb.Auth.Demo.AppHost/Program.cs
+++ src/Mystikweb.Auth.Demo.AppHost/Program.cs
@@ -1,46 +1,48 @@
+using Mystikweb.Auth.Demo;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache")
-	.WithContainerName("cache-demo")
-	.WithDataVolume("cache-data", false)
+var cache = builder.AddRedis(ServiceConstants.CacheService.RESOURCE_NAME)
+	.WithContainerName(ServiceConstants.CacheService.RESOURCE_CONTAINER_NAME)
+	.WithDataVolume(ServiceConstants.CacheService.RESOURCE_DATA_VOLUME)
 	.WithLifetime(ContainerLifetime.Persistent)
-	.WithDbGate(containerName: "identity-db-management");
+	.WithDbGate(containerName: ServiceConstants.DATABASE_MANAGEMENT_RESOURCE_NAME);
 
-var identityDbUsername = builder.AddParameter("IdentityDbUsername", true);
-var identityDbPassword = builder.AddParameter("IdentityDbPassword", true);
-var identityDbServer = builder.AddPostgres("identity", identityDbUsername, identityDbPassword, 5450)
-	.WithContainerName("identity-db-demo")
-	.WithDataVolume("identity-data", false)
+var identityDbUsername = builder.AddParameter(ServiceConstants.IdentityService.DATABASE_SERVER_USERNAME_PARAMETER, true);
+var identityDbPassword = builder.AddParameter(ServiceConstants.IdentityService.DATABASE_SERVER_PASSWORD_PARAMETER, true);
+var identityDbServer = builder.AddPostgres(ServiceConstants.IdentityService.DATABASE_SERVER_RESOURCE_NAME, identityDbUsername, identityDbPassword, ServiceConstants.IdentityService.DATABASE_SERVER_PORT)
+	.WithContainerName(ServiceConstants.IdentityService.DATABASE_SERVER_CONTAINER_NAME)
+	.WithDataVolume(ServiceConstants.IdentityService.DATABASE_SERVER_DATA_VOLUME)
 	.WithLifetime(ContainerLifetime.Persistent)
-	.WithDbGate(containerName: "identity-db-management");
+	.WithDbGate(containerName: ServiceConstants.DATABASE_MANAGEMENT_RESOURCE_NAME);
 
 var identityDb = identityDbServer
-	.AddDatabase("identitydb", "identity_db");
+	.AddDatabase(ServiceConstants.IdentityService.DATABASE_RESOURCE_NAME, ServiceConstants.IdentityService.DATABASE_NAME);
 
-var identity = builder.AddProject<Projects.Mystikweb_Auth_Demo_Identity>("identityserver")
+var identity = builder.AddProject<Projects.Mystikweb_Auth_Demo_Identity>(ServiceConstants.IdentityService.SERVER_RESOURCE_NAME)
 	.WithReference(cache)
 	.WaitFor(cache)
     .WithReference(identityDb)
 	.WaitFor(identityDb);
 
-var apiDbUsername = builder.AddParameter("ApiDbUsername", true);
-var apiDbPassword = builder.AddParameter("ApiDbPassword", true);
-var apiDbServer = builder.AddPostgres("api", apiDbUsername, apiDbPassword, 5451)
-	.WithContainerName("api-db-demo")
-	.WithDataVolume("api-data", false)
+var apiDbUsername = builder.AddParameter(ServiceConstants.ApiService.DATABASE_SERVER_USERNAME_PARAMETER, true);
+var apiDbPassword = builder.AddParameter(ServiceConstants.ApiService.DATABASE_SERVER_PASSWORD_PARAMETER, true);
+var apiDbServer = builder.AddPostgres(ServiceConstants.ApiService.DATABASE_SERVER_RESOURCE_NAME, apiDbUsername, apiDbPassword, 5451)
+	.WithContainerName(ServiceConstants.ApiService.DATABASE_SERVER_CONTAINER_NAME)
+	.WithDataVolume(ServiceConstants.ApiService.DATABASE_SERVER_DATA_VOLUME)
 	.WithLifetime(ContainerLifetime.Persistent)
-	.WithDbGate(containerName: "identity-db-management");
+	.WithDbGate(containerName: ServiceConstants.DATABASE_MANAGEMENT_RESOURCE_NAME);
 
 var apiDb = apiDbServer
-	.AddDatabase("apidb", "api_db");
+	.AddDatabase(ServiceConstants.ApiService.DATABASE_RESOURCE_NAME, ServiceConstants.ApiService.DATABASE_NAME);
 
-var apiService = builder.AddProject<Projects.Mystikweb_Auth_Demo_ApiService>("apiservice")
+var apiService = builder.AddProject<Projects.Mystikweb_Auth_Demo_ApiService>(ServiceConstants.ApiService.SERVER_RESOURCE_NAME)
     .WithReference(identity)
 	.WaitFor(identity)
 	.WithReference(apiDb)
 	.WaitFor(apiDb);
 
-builder.AddProject<Projects.Mystikweb_Auth_Demo_Web>("webfrontend")
+builder.AddProject<Projects.Mystikweb_Auth_Demo_Web>(ServiceConstants.WebService.SERVER_RESOURCE_NAME)
 	.WithExternalHttpEndpoints()
 	.WithReference(cache)
 	.WaitFor(cache)
--- src/Mystikweb.Auth.Demo/ServiceConstants.cs
+++ src/Mystikweb.Auth.Demo/ServiceConstants.cs
@@ -2,6 +2,44 @@
 
 public static class ServiceConstants
 {
+	public const string DATABASE_MANAGEMENT_RESOURCE_NAME = "identity-db-management";
 
+	public static class CacheService
+	{
+		public const string RESOURCE_NAME = "cache";
+		public const string RESOURCE_CONTAINER_NAME = "cache-demo";
+		public const string RESOURCE_DATA_VOLUME = "cache-data";
+	}
+
+	public static class IdentityService
+	{
+		public const string DATABASE_SERVER_PASSWORD_PARAMETER = "IdentityDbPassword";
+		public const string DATABASE_SERVER_USERNAME_PARAMETER = "IdentityDbUsername";
+		public const string DATABASE_SERVER_RESOURCE_NAME = "identity";
+		public const int DATABASE_SERVER_PORT = 5450;
+		public const string DATABASE_SERVER_CONTAINER_NAME = "identity-db-demo";
+		public const string DATABASE_SERVER_DATA_VOLUME = "identity-data";
+		public const string DATABASE_RESOURCE_NAME = "identitydb";
+		public const string DATABASE_NAME = "identity_db";
+		public const string SERVER_RESOURCE_NAME = "identityserver";
+	}
+
+	public static class ApiService
+	{
+		public const string DATABASE_SERVER_USERNAME_PARAMETER = "ApiDbUsername";
+		public const string DATABASE_SERVER_PASSWORD_PARAMETER = "ApiDbPassword";
+		public const string DATABASE_SERVER_RESOURCE_NAME = "api";
+		public const int DATABASE_SERVER_PORT = 5451;
+		public const string DATABASE_SERVER_CONTAINER_NAME = "api-db-demo";
+		public const string DATABASE_SERVER_DATA_VOLUME = "api-data";
+		public const string DATABASE_RESOURCE_NAME = "apidb";
+		public const string DATABASE_NAME = "api_db";
+		public const string SERVER_RESOURCE_NAME = "apiservice";
+	}
+
+	public static class WebService
+	{
+		public const string SERVER_RESOURCE_NAME = "webfrontend";
+	}
 }
 
