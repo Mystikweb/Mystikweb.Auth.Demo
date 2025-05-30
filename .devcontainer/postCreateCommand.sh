1eb51959af5f9665698386e3e644d6cbdecfabdf
Christopher Hair <mystikweb@live.ca>
2025-05-30 16:42:30 +0000

    Adding ASP.NET code generator tool to default devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Setup the ASP.NET Code Generator tool
+dotnet tool install --global dotnet-aspnet-codegenerator
+
 # Setup the Entity Framework Core CLI tool
 dotnet tool install --global dotnet-ef
 
