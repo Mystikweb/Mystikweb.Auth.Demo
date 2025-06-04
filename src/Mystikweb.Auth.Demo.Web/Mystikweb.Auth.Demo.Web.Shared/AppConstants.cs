2fc4da8d2a8b5cbf63f19ca6dbfe602b1a9925a4
Christopher Hair <mystikweb@live.ca>
2025-06-04 16:27:17 +0000

    Enhance Address Book UI with data grid and refresh functionality; add AppConstants and styles; implement RefreshButton component; update layout and routing for improved user experience.

---
+++ src/Mystikweb.Auth.Demo.Web/Mystikweb.Auth.Demo.Web.Shared/AppConstants.cs
@@ -0,0 +1,9 @@
+namespace Mystikweb.Auth.Demo.Web.Shared;
+
+public static class AppConstants
+{
+    public const string APPLICATION_CONTENT_ID = "application-content";
+    public const string MESSAGES_TOP = "TOP";
+    public const string MESSAGES_DIALOG = "DIALOG";
+    public const string MESSAGES_CARD = "CARD";
+}
